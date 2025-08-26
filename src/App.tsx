da805cf954e99a0fb35a2246ecbfb1bc7b26bd2d
kanphatratrazer <kanphatratrazer28@gmail.com>
2025-08-27 00:44:47 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-full h-screen flex items-center relative xl:p-7 py-0 px-2">
+    <div className="h-full xl:w-[calc(100%-250px)] w-[calc(100%-80px)] bg-background xl:ml-[70px] xl:rounded-2xl flex xl:flex-row fl;ex-col items-start border color-main-border/70"></div>
+    
+    </div>
+  );
+};
+export default App;
